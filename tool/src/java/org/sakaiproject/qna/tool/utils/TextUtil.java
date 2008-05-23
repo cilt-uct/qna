4cf543b1a2ca05210148d42830d7a49928e2f008
wilhelm <wilhelm@psybergate.co.za>
2008-05-23 10:26:32 +0000

    NOJIRA Whitespace in category/question text fix

--- tool/src/java/org/sakaiproject/qna/tool/utils/TextUtil.java
+++ tool/src/java/org/sakaiproject/qna/tool/utils/TextUtil.java
@@ -13,7 +13,7 @@ public class TextUtil {
 	 * @return String without tags
 	 */
 	public static String stripTags(String html) {
-		return html.replaceAll("\\<.*?>","").replaceAll("&lt;", "<").replaceAll("&gt;", ">");
+		return html.replaceAll("\\<.*?>","").replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&nbsp;", " ");
 	}
 	
 	/**
